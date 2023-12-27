0c80308c8d3fa2e3dcc3a27ed1c427c0371db0a7
Hamza <hamza.amghar@student.ehb.be>
2023-12-27 21:38:21 +0100

    birthday

--- blog/resources/views/profile/partials/update-profile-information-form.blade.php
+++ blog/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -27,7 +27,8 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
+            <label for="birthday">birthday:</label>
+            <input type="date" name="birthday" value="{{ old('birthday', auth()->user()->birthday) }}">
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
