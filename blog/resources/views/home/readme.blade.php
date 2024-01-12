9955feda22dba8995e8b3ca44cbad2541f6e3574
Hamza <hamza.amghar@student.ehb.be>
2024-01-12 18:10:54 +0100

    list sources update

--- blog/resources/views/home/readme.blade.php
+++ blog/resources/views/home/readme.blade.php
@@ -12,13 +12,16 @@
       </div>
       <!-- header section end -->
       <!-- services section start -->
+      
       <div class="container mt-5 mb-5">
-         <h2 style="padding-bottom: 30px !important">List of sources:</h2>
+         <h2 style="padding-bottom: 40px !important">List  sources:</h2>
          <ul class="list-group">
             <li class="list-group-item"><a href="https://www.youtube.com/@WebTechKnowledge" target="_blank"> Web Tech Knowledge</a></li>
             <li class="list-group-item"><a href="https://www.youtube.com/watch?v=dxt7w5NUub8&list=PLAyPO7TEH9XdZimg1YDJA7-jHPr0CzQo9" target="_blank"> Parfaitement Web</a></li>
             <li class="list-group-item"><a href="https://chat.openai.com/" target="_blank">Chat GPT</a></li>
             <li class="list-group-item"><a href="https://canvas.ehb.be/courses/34801/assignments/145251" target="_blank">Canvas</a></li>
+            <li class="list-group-item"><a href="https://github.com/yaminshakil/Blog_Template" target="_blank">Github user template </a></li>
+            <li class="list-group-item"><a href="https://github.com/yaminshakil/Admin_Template" target="_blank">Github Admin template</a></li>
             <li class="list-group-item">Medestudenten</a></li>
             
          </ul>
