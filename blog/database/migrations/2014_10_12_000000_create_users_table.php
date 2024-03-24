9ee3f275d6788f9f98566be71b01a7d351e77ecd
Hamza <hamza.amghar@student.ehb.be>
2024-03-24 15:45:56 +0100

    Update 2014_10_12_000000_create_users_table.php

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('usertype')->default('user');
             $table->text('biography')->nullable(); 
-            $table->date('birthday')->nullable(); 
+           
             $table->rememberToken();
             $table->timestamps();
         });
