67e12b526f824e265ea77db4d782927daa7d2cc9
Hamza <hamza.amghar@student.ehb.be>
2024-03-24 16:42:06 +0100

    update about

--- blog/resources/views/home/readme.blade.php
+++ blog/resources/views/home/readme.blade.php
@@ -19,9 +19,10 @@
             <li class="list-group-item"><a href="https://www.youtube.com/@WebTechKnowledge" target="_blank"> Web Tech Knowledge</a></li>
             <li class="list-group-item"><a href="https://www.youtube.com/watch?v=dxt7w5NUub8&list=PLAyPO7TEH9XdZimg1YDJA7-jHPr0CzQo9" target="_blank"> Parfaitement Web</a></li>
             <li class="list-group-item"><a href="https://chat.openai.com/" target="_blank">Chat GPT</a></li>
-            <li class="list-group-item"><a href="https://canvas.ehb.be/courses/34801/assignments/145251" target="_blank">Canvas</a></li>
+            <li class="list-group-item"><a href="https://canvas.ehb.be/courses/34801/files/2523688?module_item_id=572459" target="_blank">Canvas</a></li>
             <li class="list-group-item"><a href="https://github.com/yaminshakil/Blog_Template" target="_blank">Github user template </a></li>
             <li class="list-group-item"><a href="https://github.com/yaminshakil/Admin_Template" target="_blank">Github Admin template</a></li>
+            <li class="list-group-item"><a href="https://github.com/Hamza-180/Blog" target="_blank">Github repo</a></li>
             <li class="list-group-item">Medestudenten</a></li>
             
          </ul>
