00e06971a2c196d618baf9691beaf8b6c50eed82
Hamza <hamza.amghar@student.ehb.be>
2024-04-04 17:30:18 +0200

    Route
    Route meer gestructureerd

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -6,58 +6,45 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController; 
 use App\Http\Controllers\FaqController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
+// Homepage route
 Route::get('/', [HomeController::class,'homepage']);
 
-
+// Homepage with auth middleware
 route::get('/home', [HomeController::class,'index'])->middleware('auth')->name('home');
 
+// Post page with auth and admin middleware
+Route::get('post', [HomeController::class,'post'])->middleware(['auth','admin']);
 
-Route::get('post', [HomeController::class,'post'])->
-middleware([ 'auth','admin']);
-
+// User profile routes with auth middleware
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Include auth routes
 require __DIR__.'/auth.php';
 
-
+// Admin routes for post management
 Route::get('/post_page', [AdminController::class,'post_page']);
-
 Route::post('/add_post', [AdminController::class,'add_post']);
-
 Route::get('/show_post', [AdminController::class,'show_post']);
-
 Route::get('/delete_post/{id}', [AdminController::class,'delete_post']);
-
 Route::get('/edit_page/{id}', [AdminController::class,'edit_page']);
-
 Route::post('/update_post/{id}', [AdminController::class,'update_post']);
 
+// View post details
 Route::get('/post_details/{id}', [HomeController::class,'post_details']);
 
+// Create post route with auth middleware
 Route::get('/create_post', [HomeController::class,'create_post']) ->middleware('auth');
 
+// Store user's post
 Route::post('/user_post', [HomeController::class,'user_post']);
 
-
-
-Route::get('/contact', [HomeController::class,'contact']) ;  
-
-Route::get('/readme', [HomeController::class,'readme']) ;  
-
-Route::get('/faq', [HomeController::class,'faq']) ;  
-
-
+// Routes for contact, readme and faq
+Route::get('/contact', [HomeController::class,'contact']);
+Route::get('/readme', [HomeController::class,'readme']);
+Route::get('/faq', [HomeController::class,'faq']);
